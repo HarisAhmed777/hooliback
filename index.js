29be3fa6e11b136bfe155923ff85c371188026a8
HarisAhmed777 <ahmedharis371@gmail.com>
2024-06-21 14:16:50 +0530

    loading4

--- index.js
+++ index.js
@@ -306,7 +306,7 @@ app.post("/resetpassword/:_id/:token", async (req, res) => {
 });
 
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build/index.html'));
   }); 
   
